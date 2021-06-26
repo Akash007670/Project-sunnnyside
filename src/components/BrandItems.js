4a83f23bd7b43992d23a9beb92b93d97042235f3
Akash Kumar <akaysharma894@gmail.com>
2021-06-27 00:01:37 +0530

    Added branditem

---
+++ src/components/BrandItems.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./BrandItems.css";
+
+const BrandItems = () => {
+  return (
+    <div className="brands">
+      <div className="box1">
+        <h1>Tranform your brand</h1>
+        <p>
+          We are a full-service creative agency specializing in helping brands
+          grow fast. Engage your clients through compellings visuals that do
+          most of the marketing for you.
+        </p>
+        <h3>LEARN MORE</h3>
+      </div>
+      <div className="box2">World</div>
+      <div className="box3">Hey</div>
+      <div className="box4">
+        <h1>Stand out to the right audience</h1>
+        <p>
+          We are a full-service creative agency specializing in helping brands
+          grow fast. Engage your clients through compellings visuals that do
+          most of the marketing for you.
+        </p>
+        <h3>LEARN MORE</h3>
+      </div>
+    </div>
+  );
+};
+
+export default BrandItems;
